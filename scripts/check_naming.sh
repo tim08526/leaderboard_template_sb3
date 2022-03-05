133833d0d3392fdd595b9613b7ae0ec1c63043ed
mlap <marcuslapeyrolerie@me.com>
2022-03-05 00:03:23 +0000

    Deleting dirs

--- scripts/check_naming.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-python3 -m pytest --cov-config .coveragerc --cov-report html --cov-report term --cov=. -v tests/*.py
-pip install pandas
-
-pip install -r requirements.txt
-
-python python/score_model.py -d "."
