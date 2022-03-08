4d6cae0f8c9f12f4499788e1be0cb478adb0b17e
mlap <marcuslapeyrolerie@me.com>
2022-03-08 01:39:24 +0000

    Some streamlining

--- utils/scripts/check_naming.sh
+++ utils/scripts/check_naming.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 python3 -m pytest --cov-config .coveragerc --cov-report html --cov-report term --cov=. -v utils/tests/*.py
 
-pip install pandas
-
-pip install -r requirements.txt
-
-python utils/python/score_model.py -d "."
