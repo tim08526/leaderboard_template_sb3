f169bf05320e4903fab7acf2599c8b449f1799ea
mlap <marcuslapeyrolerie@me.com>
2022-03-09 02:40:28 +0000

    Adding line to check for file existence

--- utils/tests/test-naming.py
+++ utils/tests/test-naming.py
@@ -3,6 +3,7 @@ import re
 
 def test_naming():
     models = glob.glob("*.zip")
+    assert models != [], "Model(s) not found"
     for model in models:
         parsed = re.search("([a-zA-Z]+\-v\d+)\-([A-Z0-9]+)\-(\w+)\.zip", model)
         env_name = parsed.group(1)
